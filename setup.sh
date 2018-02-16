db906f7273b2dfd17021ed238af33d266cd3dac7
Ryan Roark <ryan.roark@parivedasolutions.com>
2018-02-16 13:22:48 -0600

    Adding commentary to setup script

--- setup.sh
+++ setup.sh
@@ -1,2 +1,4 @@
+#Setup configuration files using hardlinks - you can change file in either location, will update this repo - just be sure to check in occassionally
+
 ln i3config ~/.config/i3/config
 ln .vimrc ~/.vimrc
