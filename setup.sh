911f02d9a82b5cf29c863b5133635e26b4abd415
Ryan Roark <roarkry@gmail.com>
2018-02-27 21:09:07 -0600

    blur lock

--- setup.sh
+++ setup.sh
@@ -4,3 +4,5 @@ ln i3config ~/.config/i3/config
 ln vimrc ~/.vimrc
 ln i3blocks.conf ~/.i3blocks.conf
 ln bash_aliases ~/.bash_aliases
+ln blurLock.sh ~/.blurLock.sh
+sudo apt install feh compton fonts-font-awesome fonts-roboto i3blocks scrot imagemagick xautolock
