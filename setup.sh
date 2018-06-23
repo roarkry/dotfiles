cbe6dc8530465066c3b3b07897e7d32a400d66e0
Ryan Roark <roarkry@gmail.com>
2018-06-22 21:50:27 -0500

    updating setup notes:

--- setup.sh
+++ setup.sh
@@ -6,3 +6,4 @@ ln i3blocks.conf ~/.i3blocks.conf
 ln bash_aliases ~/.bash_aliases
 ln blurLock.sh ~/.blurLock.sh
 sudo apt install feh compton fonts-font-awesome fonts-roboto i3blocks scrot imagemagick xautolock xss-lock rofi
+echo "Need to install gaps(link in i3config) and polybar(getdeb repo) manually"
